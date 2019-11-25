cc872a2b1fa88c9b1cdccfce4db6309783779e41
qlshi <619629156@qq.com>
2019-11-25 23:52:25 +0800

    whitebord update

--- app/src/main/java/com/activity/WhiteBoardActivity.java
+++ app/src/main/java/com/activity/WhiteBoardActivity.java
@@ -1,6 +1,8 @@
 package com.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.BaseActivity.BaseActivity;
@@ -8,7 +10,6 @@ import com.greendaodemo2.R;
 import com.widget.MyWhiteBoard;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class WhiteBoardActivity extends BaseActivity {
@@ -16,6 +17,22 @@ public class WhiteBoardActivity extends BaseActivity {
     Button undo;
     @BindView(R.id.whiteboard)
     MyWhiteBoard whiteboard;
+    @BindView(R.id.reundo)
+    Button reundo;
+    @BindView(R.id.scale)
+    Button scale;
+    @BindView(R.id.reduce)
+    Button reduce;
+    @BindView(R.id.red)
+    Button red;
+    @BindView(R.id.blue)
+    Button blue;
+    @BindView(R.id.green)
+    Button green;
+    @BindView(R.id.gray)
+    Button gray;
+    @BindView(R.id.eraser)
+    Button eraser;
 
     @Override
     protected void initView(Bundle savedInstanceState) {
@@ -26,5 +43,38 @@ public class WhiteBoardActivity extends BaseActivity {
     @OnClick(R.id.undo)
     public void onViewClicked() {
         whiteboard.undo();
+    }
+
+
+    @OnClick(R.id.reundo)
+    public void onClick() {
+        whiteboard.reundo();
+    }
+
+    @OnClick({R.id.scale, R.id.reduce, R.id.red, R.id.blue, R.id.green, R.id.gray,R.id.eraser})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.scale:
+                whiteboard.scalePaintWidth();
+                break;
+            case R.id.reduce:
+                whiteboard.reducePaintWidth();
+                break;
+            case R.id.red:
+                whiteboard.resetPaintColor(getResources().getColor(R.color.red_pressed));
+                break;
+            case R.id.blue:
+                whiteboard.resetPaintColor(getResources().getColor(R.color.blue));
+                break;
+            case R.id.green:
+                whiteboard.resetPaintColor(getResources().getColor(R.color.green_pressed));
+                break;
+            case R.id.gray:
+                whiteboard.resetPaintColor(getResources().getColor(R.color.gray));
+                break;
+            case R.id.eraser:
+                whiteboard.eraser(Color.WHITE);
+                break;
+        }
     }
 }
--- app/src/main/java/com/widget/MyWhiteBoard.java
+++ app/src/main/java/com/widget/MyWhiteBoard.java
@@ -2,6 +2,7 @@ package com.widget;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.support.annotation.Nullable;
@@ -18,15 +19,16 @@ public class MyWhiteBoard extends View {
     private float downX, downY;
     private float tempX, tempY;
     private List<DrawPath> drawPathList = new ArrayList<>();
+    private List<DrawPath> savePathList=new ArrayList<>();
+    private float paintWidth=10;
+    private int mPaontColor=Color.BLACK;
 
     public MyWhiteBoard(Context context) {
-        super(context);
-        initPaint();
+        this(context,null);
     }
 
     public MyWhiteBoard(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        initPaint();
+        this(context, attrs,0);
     }
 
     public MyWhiteBoard(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -37,13 +39,15 @@ public class MyWhiteBoard extends View {
     private void initPaint() {
         paint = new Paint();
         paint.setAntiAlias(true);
-        paint.setStrokeWidth(10);
+        paint.setStrokeWidth(paintWidth);
+        paint.setColor(mPaontColor);
         paint.setStyle(Paint.Style.STROKE);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
         if(drawPathList!=null&&!drawPathList.isEmpty()){
             for(DrawPath drawPath:drawPathList){
                 if(drawPath!=null){
@@ -85,10 +89,62 @@ public class MyWhiteBoard extends View {
         return true;
     }
     public void undo(){
-        if(drawPathList!=null&&drawPathList.size()>1){
+        if(drawPathList!=null&&drawPathList.size()>0){
+            savePathList.add(drawPathList.get(drawPathList.size()-1));
             drawPathList.remove(drawPathList.size()-1);
             invalidate();
         }
+    }
+    /**
+     * 反撤销功能
+     */
+    public void reundo() {
+        if(savePathList!=null&&!savePathList.isEmpty()){
+            drawPathList.add(savePathList.get(savePathList.size()-1));
+            savePathList.remove(savePathList.size()-1);
+            invalidate();
+        }
+    }
+    /**
+     * 改变画笔颜色
+     * @param color
+     */
+    public void resetPaintColor(int color) {
+        mPaontColor=color;
+        paint.setColor(color);
+    }
+    /**
+     * 放大就是改变画笔的宽度
+     */
+    public void scalePaintWidth() {
+        paintWidth+=2;
+        paint.setStrokeWidth(paintWidth);
+    }
+    /**
+     * 缩小就是改变画笔的宽度
+     */
+    public void reducePaintWidth() {
+        if(paintWidth>2){
+            paintWidth-=2;
+        }
+        paint.setStrokeWidth(paintWidth);
+    }
+
+    /**
+     * 设置画笔宽度
+     */
+    public void setPaintWidth(float paintWidth){
+        this.paintWidth=paintWidth;
+        paint.setStrokeWidth(paintWidth);
+    }
+    /**
+     * 橡皮擦功能 把画笔的颜色和view的背景颜色一样就ok,然后把画笔的宽度变大了,擦除的时候显得擦除范围大点
+     */
+    public void eraser(int bgcolor) {
+        mPaontColor=bgcolor;
+        paint.setColor(bgcolor);//这是view背景的颜色
+        paintWidth=paintWidth+10;
+        paint.setStrokeWidth(paintWidth);
     }
 
     class DrawPath {
