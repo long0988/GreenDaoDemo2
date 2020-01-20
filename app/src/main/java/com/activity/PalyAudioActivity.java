7733943c2aea890a5572362c05b52fbdbc6472cf
shiqilong <shiqilong@skyworth.com>
2020-01-20 09:49:08 +0800

    换行RadioGroup

---
+++ app/src/main/java/com/activity/PalyAudioActivity.java
@@ -0,0 +1,19 @@
+package com.activity;
+
+import android.os.Bundle;
+
+import com.BaseActivity.BaseActivity;
+import com.greendaodemo2.R;
+
+public class PalyAudioActivity extends BaseActivity {
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        super.initView(savedInstanceState);
+        setContentView(R.layout.activity_play_audio);
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+    }
+}
